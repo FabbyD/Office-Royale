92ab33add8c66f3ad7d6a379e92e695e8a84efc6
Fabrice Dugas <faberice.dugas@gmail.com>
2018-07-30 16:44:31 -0400

    Dev stuff

---
+++ Assets/Scripts/NetworkClock.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System;
+
+public class NetworkClock : NetworkBehaviour {
+
+    private NetworkClock instance;
+    private long ping;
+    public float msOffset = 0;
+
+    private void Awake()
+    {
+        // Singleton pattern
+        if (instance == null)
+        {
+            instance = this;
+        } else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        //Sets this to not be destroyed when reloading scene
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public override void OnStartClient()
+    {
+        if (!isServer)
+        {
+            ping = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+            CmdPing();
+            
+        }
+    }
+    
+    
+    [Command]
+    private void CmdPing()
+    {
+        RpcPong(DateTimeOffset.UtcNow.ToUnixTimeMilliseconds());
+    }
+
+    [ClientRpc]
+    private void RpcPong(long pong)
+    {
+        long now = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+        long round = now - ping;
+        long oneway = round / 2;
+        msOffset = ping + oneway - pong;
+        Debug.Log("RpcPong: " + msOffset);
+    }
+}
--- Assets/Scripts/SafeZone.cs
+++ Assets/Scripts/SafeZone.cs
@@ -1,32 +1,28 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
+using System;
 
 public class SafeZone : NetworkBehaviour {
 
-    public float shrinkSpeed = 0.2f;
+    public float shrinkSpeed = 0.5f;
 
     public float moveSpeed = 1;
 
+    [SyncVar]
     public float targetSize = 3;
 
-    public Vector2 targetPosition;
+    [SyncVar]
+    public Vector2 targetPosition = new Vector2(-12,-18);
 
-    private SpriteMask spriteMask;
-
-	// Use this for initialization
-	void Start () {
-        spriteMask = GetComponent<SpriteMask>();
-        targetPosition = new Vector2(-12,-18);
-	}
+    [SyncVar]
+    public float shrinkStart;
 	
 	void FixedUpdate () {
-        if (!isServer)
+        if (isServer)
         {
-            return;
+            shrink();
+            move();
         }
-
-        shrink();
-        move();
 	}
 
     private void shrink()
@@ -39,7 +35,6 @@ public class SafeZone : NetworkBehaviour {
 
     private void move()
     {
-
         // The move animation should take as long as the shrink animation unless only the target position changed
         float moveSpeed = this.moveSpeed;
         float deltaSize = transform.localScale.x - targetSize;
