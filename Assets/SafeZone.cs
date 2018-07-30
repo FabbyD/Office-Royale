e0d3feca2a7f72208bec24a8891b38eb923cc974
Fabrice Dugas <faberice.dugas@gmail.com>
2018-07-30 15:09:48 -0400

    Add wall of death

---
+++ Assets/SafeZone.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class SafeZone : NetworkBehaviour {
+
+    public float shrinkSpeed = 0.2f;
+
+    public float moveSpeed = 1;
+
+    public float targetSize = 3;
+
+    public Vector2 targetPosition;
+
+    private SpriteMask spriteMask;
+
+	// Use this for initialization
+	void Start () {
+        spriteMask = GetComponent<SpriteMask>();
+        targetPosition = new Vector2(-12,-18);
+	}
+	
+	void FixedUpdate () {
+        if (!isServer)
+        {
+            return;
+        }
+
+        shrink();
+        move();
+	}
+
+    private void shrink()
+    {
+        transform.localScale = Vector2.MoveTowards(
+            transform.localScale,
+            new Vector2(targetSize, targetSize),
+            shrinkSpeed * Time.deltaTime);
+    }
+
+    private void move()
+    {
+
+        // The move animation should take as long as the shrink animation unless only the target position changed
+        float moveSpeed = this.moveSpeed;
+        float deltaSize = transform.localScale.x - targetSize;
+        if (deltaSize > 0)
+        {
+            float time = deltaSize / shrinkSpeed;
+            float distance = (targetPosition - (Vector2)transform.position).magnitude;
+            moveSpeed = distance / time;
+        }
+        transform.position = Vector2.MoveTowards(
+            transform.position,
+            targetPosition,
+            moveSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/SafeZoneCheck.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SafeZoneCheck : MonoBehaviour {
+
+    public int damage = 5;
+    public int damageRate = 1;
+
+    private Health health;
+    private GameObject safeZone;
+    public bool isSafe = true;
+    private float lastDamageTick = 0;
+
+	// Use this for initialization
+	void Start () {
+        health = GetComponent<Health>();
+        safeZone = GameObject.FindGameObjectWithTag("SafeZone");
+	}
+
+    void Update()
+    {
+        if (!isSafe && lastDamageTick >= damageRate)
+        {
+            lastDamageTick = 0;
+            health.TakeDamage(damage);
+        } else
+        {
+            lastDamageTick += Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.name == "Safe Zone")
+        {
+            isSafe = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject.name == "Safe Zone")
+        {
+            isSafe = false;
+        }
+    }
+}
