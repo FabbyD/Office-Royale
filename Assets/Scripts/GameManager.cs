0ceb512083b1423fd6364d7162683f662669c8f6
Fabrice Dugas <faberice.dugas@gmail.com>
2018-08-01 17:16:15 -0400

    Add GameManager and UIOverlay

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance;         //A reference to our game manager script so we can access it statically
+
+    public int eliminations = 0;
+
+    void Awake()
+    {
+        //If we don't currently have a game manager...
+        if (instance == null)
+            //...set this one to be it...
+            instance = this;
+        //...otherwise...
+        else if (instance != this)
+            //...destroy this one because it is a duplicate.
+            Destroy(gameObject);
+    }
+
+    public void AddElimination()
+    {
+        eliminations++;
+    }
+
+}
